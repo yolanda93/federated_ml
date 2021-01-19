b91d83d92fd2aecd7c3ca6e1e01fc2e03a37e1e5
yolanda93 <yolanda.delahoz.next@bbva.com>
2021-01-19 21:45:17 +0100

    finished test_connection

--- tests/utils/operations.py
+++ tests/utils/operations.py
@@ -2,16 +2,19 @@ import asyncio
 import torch
 
 def check_tensor(worker):
+    # Prueba 1: test connection
     x_valid = torch.tensor([-1, 2.0]).tag("x")
     x_server = worker.search("x")[0]
     return x_valid, x_server
 
 async def check_tensor(worker):
+    # Prueba 1: test connection
     x_valid, x_server = check_tensor(worker)
     await asyncio.sleep(0)
     return torch.equal(x_server, x_valid)
 
 async def sum_tensor(worker):
+    # Prueba 3: test connection
     await asyncio.sleep(0)
     x = torch.ones(5)
     x = x.send(worker)
